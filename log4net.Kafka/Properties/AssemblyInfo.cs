593a8a98b10a94872e5865a91a6e43449c8ee141
Tim Tran <quangthuant@gmail.com>
2019-03-21 10:15:58 -0500

    change assemby name

--- log4net.Kafka/Properties/AssemblyInfo.cs
+++ log4net.Kafka/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("log4net.Kafka")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("log4net.Kafka.LogtashLayout")]
+[assembly: AssemblyDescription("log4net appender to Kafka and provide logstash json_event PatternLayout with LoggingEvent's properties")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("log4net.Kafka")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.4")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
